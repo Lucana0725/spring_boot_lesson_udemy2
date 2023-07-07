7a2b9df614b326ee5695ae9f94c05cba2ac376c1
Lucana <k.ark.scarlet72@gmail.com>
2023-07-07 23:44:39 +0900

    [Finish]SurveyController.javaにてsurvey/confirmページへの繊維関係の処理を実装

--- src/main/java/com/example/demo/app/survey/SurveyController.java
+++ src/main/java/com/example/demo/app/survey/SurveyController.java
@@ -2,7 +2,10 @@ package com.example.demo.app.survey;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,5 +18,15 @@ public class SurveyController {
 		return "survey/form";
 	}
 	
-	
+	@PostMapping("/confirm")
+	public String confirm(@Validated SurveyForm surveyForm, 
+			BindingResult result, 
+			Model model) {
+		if(result.hasErrors()) {
+			model.addAttribute("title", "Survey Form");
+			return "survey/form";
+		}
+		model.addAttribute("title", "Confirm Page");
+		return "survey/confirm";
+	}
 }
