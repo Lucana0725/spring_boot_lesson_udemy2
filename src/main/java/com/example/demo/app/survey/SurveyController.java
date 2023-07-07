406513e22670522cc6e3c54bfbf73177faa24fd1
Lucana <k.ark.scarlet72@gmail.com>
2023-07-07 21:41:54 +0900

    [Finish]SurveyControllerで/survey/formアクセス用のメソッドを作成

--- src/main/java/com/example/demo/app/survey/SurveyController.java
+++ src/main/java/com/example/demo/app/survey/SurveyController.java
@@ -1,5 +1,11 @@
 package com.example.demo.app.survey;
 
-public class SurveyController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/survey")
+public class SurveyController {
+	
+	
 }
